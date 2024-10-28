4fc036290e48f819a676ca1da17dd6194f418262
ahnsv <ahnsv@bc.edu>
2024-10-28 17:24:44 +0000

    [lc] Runtime - 89 ms (5.09%), Memory - 25.9 MB (100.00%)

--- lc/0121-best-time-to-buy-and-sell-stock/solution.py
+++ lc/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,16 +1,12 @@
 class Solution:
-    cache = []
     def maxProfit(self, prices: List[int]) -> int:
-        self.cache.clear()
-        if len(prices) == 0:
-            return 0
-        for i, v in enumerate(prices):
-            if v < max(prices[i:]):
-                profit = max(prices[i:]) - v
-                if profit not in self.cache: 
-                    self.cache.append(profit)
-        if len(self.cache) == 0:
-            return 0
-        print(self.cache)
-        return sorted(self.cache, reverse=True)[0]
+        min_price = 999999999
+        max_profit = -9999999
+
+        for _, price in enumerate(prices):
+            min_price = min(min_price, price)
+            if max_profit < price - min_price:
+                max_profit = price - min_price
             
+        
+        return max_profit
