63e2e80aea1dea2700cf9111b97b9330469131d6
ahnsv <ahnsv@bc.edu>
2024-11-04 15:27:27 +0000

    [lc] Runtime - 0 ms (100.00%), Memory - 17.6 MB (100.00%)

--- lc/0792-binary-search/solution.py
+++ lc/0792-binary-search/solution.py
@@ -1,15 +1,16 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        start, end = 0, len(nums) - 1
-
-        while start <= end:
-            mid = (start + end) // 2
+        high, low = len(nums) - 1, 0
+        while low <= high:
+            mid = low + ((high - low) // 2)
 
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                start = mid + 1
-            elif nums[mid] > target:
-                end = mid - 1
-        
+            
+            if nums[mid] < target:
+                low = mid + 1
+            
+            if nums[mid] > target:
+                high = mid - 1
+            
         return -1
