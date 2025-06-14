678aaad8965a94cc42d86fcf0156783c40383dab
ahnsv <ahnsv@bc.edu>
2025-06-14 01:51:02 +0000

    [lc] Runtime - 10 ms (99.77%), Memory - 31.9 MB (5.92%)

--- lc/0053-maximum-subarray/solution.py
+++ lc/0053-maximum-subarray/solution.py
@@ -1,12 +1,17 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        total_sum = nums[0]
-        current_sum = nums[0]
 
-        for num in nums[1:]:
-            current_sum = max(num, current_sum + num)
-            total_sum = max(total_sum, current_sum)
-        return total_sum
+        curr_sum = 0
+        max_sum = float('-inf')
+
+        for num in nums:
+            curr_sum += num
             
-
+            if curr_sum > max_sum:
+                max_sum = curr_sum
+            
+            if curr_sum < 0:
+                curr_sum = 0
         
+        return max_sum
+
