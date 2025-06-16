591cc33b67c4341092a2e5879870bbb498299fea
ahnsv <ahnsv@bc.edu>
2025-06-16 17:10:38 +0000

    [lc] Runtime - 39 ms (90.03%), Memory - 17.8 MB (100.00%)

--- lc/0190-reverse-bits/solution.py
+++ lc/0190-reverse-bits/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
         for _ in range(32):
-            bit = n & 1
-            result = (result << 1) | bit
+            last = n & 1
+            result = (result << 1) | last
             n = n >> 1
         return result
