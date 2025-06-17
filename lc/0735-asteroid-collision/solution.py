7ffd51d1fdbbd6a5663702cfd26490289fcc2854
ahnsv <ahnsv@bc.edu>
2025-06-17 19:29:04 +0000

    [lc] Runtime - 7 ms (58.12%), Memory - 18.9 MB (100.00%)

---
+++ lc/0735-asteroid-collision/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        # stack usage
+        s = []
+        for a in asteroids:
+            while s and s[-1] > 0 and a < 0:
+                if s[-1] + a > 0: break
+                elif s[-1] + a == 0: s.pop(); break
+                else: s.pop()
+            else: s.append(a)
+        
+        return s
+
+
