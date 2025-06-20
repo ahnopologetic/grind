a2f8d7699555cb2dd0fea606d3173175a2d5a0ff
ahnsv <ahnsv@bc.edu>
2025-06-20 02:55:25 +0000

    [lc] Runtime - 24 ms (91.73%), Memory - 31.5 MB (100.00%)

---
+++ lc/2664-maximize-greatness-of-an-array/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximizeGreatness(self, nums: List[int]) -> int:
+        nums.sort()
+        ans = 0
+        for num in nums:
+            if nums[ans] < num:
+                ans += 1
+        
+        return ans
+        
+
+        
