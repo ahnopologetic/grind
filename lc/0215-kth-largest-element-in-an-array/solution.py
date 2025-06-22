24d9f35d272baa8cffca7d98f1e67656bff24e18
ahnsv <ahnsv@bc.edu>
2025-06-22 14:31:35 +0000

    [lc] Runtime - 107 ms (27.85%), Memory - 28.9 MB (99.67%)

--- lc/0215-kth-largest-element-in-an-array/solution.py
+++ lc/0215-kth-largest-element-in-an-array/solution.py
@@ -1,15 +1,10 @@
 import heapq
-
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        curr_max = -999999
-        hp = []
-        heapq.heapify(hp)
-        for _, val in enumerate(nums):
-            curr = val
-            if val > curr_max:
-                heapq.heappush(hp, val)
-            if len(hp) > k:
-                heapq.heappop(hp)
-        return heapq.heappop(hp)
-
+        result = []
+        for num in nums:
+            heapq.heappush(result, num)
+            if len(result) > k:
+                heapq.heappop(result)
+        
+        return result[0]
