ce13900540471cb80dc97a017cf6b3f3a6dc4426
ahnsv <ahnsv@bc.edu>
2025-06-28 13:09:59 +0000

    [lc] Runtime - 36 ms (95.15%), Memory - 17.9 MB (100.00%)

---
+++ lc/1894-merge-strings-alternately/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = ""
+
+        m, n = len(word1), len(word2)
+
+        for i in range(max(m, n)):
+            if i < m:
+                result += word1[i]
+                
+            if i < n:
+                result += word2[i]
+        
+        return result
