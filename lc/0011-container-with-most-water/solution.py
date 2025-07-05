14805bd1cd78e3ceadfb6922b3306d84276189af
ahnsv <ahnsv@bc.edu>
2025-07-05 10:47:21 +0000

    [lc] Runtime - 107 ms (5.71%), Memory - 28.5 MB (99.85%)

--- lc/0011-container-with-most-water/solution.py
+++ lc/0011-container-with-most-water/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         l, r = 0, len(height) - 1
-        max_area = 0
-        while l < r:
-            max_area = max(max_area, (r - l) * min(height[l], height[r]) )
+        max_area = float('-inf')
 
+        while l <= r:
+            max_area = max(max_area, (r - l) * min(height[r], height[l]))
             if height[l] < height[r]:
                 l += 1
             else:
