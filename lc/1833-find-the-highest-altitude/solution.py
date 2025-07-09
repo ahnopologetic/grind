9ff7a02d9862f1280b7c89881c7501f1ceefe21d
ahnsv <ahnsv@bc.edu>
2025-07-09 23:24:45 +0000

    [lc] Runtime - 0 ms (100.00%), Memory - 17.6 MB (100.00%)

---
+++ lc/1833-find-the-highest-altitude/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        result = [0]
+        for i in range(len(gain)):
+            result.append(gain[i] + result[-1])
+        
+        return max(result)
