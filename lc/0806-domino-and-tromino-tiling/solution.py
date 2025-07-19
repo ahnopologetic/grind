6e4f09a4c4e0d3d75f36e31286e4faed89bad679
ahnsv <ahnsv@bc.edu>
2025-07-19 17:27:33 +0000

    [lc] Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ lc/0806-domino-and-tromino-tiling/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        MOD = 10**9 + 7
+        
+        if n < 2:
+            return 1
+        
+        if n == 2:
+            return 2
+        
+        if n == 3:
+            return 5
+        
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 5
+
+        if n < 4:
+            return dp[n]
+        
+        for i in range(4, n+1):
+            dp[i] = (dp[i-1] * 2 + dp[i-3]) % MOD
+        
+        return dp[-1]
