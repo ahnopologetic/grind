77f0a0a7e1f8ac4aca47654bad52e8863ec68532
ahnsv <ahnsv@bc.edu>
2025-09-15 18:23:37 +0000

    [lc] Runtime - 7 ms (81.24%), Memory - 18.2 MB (100.00%)

--- lc/0125-valid-palindrome/solution.py
+++ lc/0125-valid-palindrome/solution.py
@@ -1,13 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        word = ''
-        for i in s:
-            if i.isalnum():
-                word += i.lower()
-
-        reverse = ''
-        for i in range(len(word) - 1, -1, -1):
-            reverse = reverse + word[i]
-
-        return word == reverse
+        s = s.lower().replace(' ', '')
+        new_s = ""
+        for ch in s:
+            if ch.isalnum():
+                new_s += ch
         
+        left, right = 0, len(new_s) - 1
+        ans = True
+        while left < right:
+            if new_s[left] != new_s[right]:
+                ans = False
+                break
+            else:
+                left += 1
+                right -= 1
+
+        return ans
