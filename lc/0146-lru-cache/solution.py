258e439b79556d9ed5d4c2af13bbd8417c3baa76
ahnsv <ahnsv@bc.edu>
2025-11-07 16:34:45 +0000

    [lc] Runtime - 157 ms (17.42%), Memory - 77.3 MB (38.89%)

--- lc/0146-lru-cache/solution.py
+++ lc/0146-lru-cache/solution.py
@@ -1,27 +1,55 @@
-from collections import OrderedDict
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
 
 class LRUCache:
 
+
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self._cache = OrderedDict()
+        self.c = capacity
+        self.h = {}
+        self.head = Node(-1, -1)
+        self.tail = Node(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
         
 
     def get(self, key: int) -> int:
-        if key not in self._cache:
+        if key not in self.h:
             return -1
-        self._cache.move_to_end(key)
-        return self._cache[key]
         
+        node = self.h[key]
+        self.remove(node)
+        self.add(node)
+        return node.val
 
     def put(self, key: int, value: int) -> None:
-        if key in self._cache:
-            self._cache.move_to_end(key)
-        self._cache[key] = value
-        if len(self._cache) > self.capacity:
-            self._cache.popitem(False)
-        
-        
+        if key in self.h:
+            old_node = self.h[key]
+            self.remove(old_node)
+        node = Node(key, value)
+        self.h[key] = node
+        self.add(node)
+
+        if len(self.h) > self.c:
+            to_delete = self.head.next
+            self.remove(to_delete)
+            del self.h[to_delete.key]
+    
+    def add(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
+    
+    
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
         
 
 
