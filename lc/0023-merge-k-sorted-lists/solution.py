2289a8269e5196e71ec8d2161b6f562e1cdd00bb
ahnsv <ahnsv@bc.edu>
2025-11-08 02:03:13 +0000

    [lc] Runtime - 11 ms (53.84%), Memory - 20.4 MB (99.99%)

--- lc/0023-merge-k-sorted-lists/solution.py
+++ lc/0023-merge-k-sorted-lists/solution.py
@@ -3,23 +3,50 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# from collections import deque
 import heapq
+
+class HeapNode:
+    def __init__(self, node):
+        self.node = node
+    
+    def __lt__(self, other):
+        return self.node.val < other.node.val
+
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        result = []
+        # nodes = []
+        # head = tail = ListNode(None)
+        # for l in lists:
+        #     while l:
+        #         nodes.append(l.val)
+        #         l= l.next
+        
+        # nodes = sorted(nodes)
+        # for node in nodes:
+        #     tail.next = ListNode(node)
+        #     tail = tail.next
+        
+        # return head.next
+        h = []
+        head = tail = ListNode(0)
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(h, (lists[i].val, i, lists[i]))
 
-        for head in lists:
-            # curr = head
-            while head:
-                heapq.heappush(result, head.val)
-                head = head.next
-        dummy = ListNode()
-        curr = dummy
-        while result:
-            curr.next = ListNode(heapq.heappop(result))
-            curr = curr.next
+        while h:
+            node = heapq.heappop(h)
+            node = node[2]
+            tail.next = node
+            tail = tail.next
+            if node.next:
+                i+=1
+                heapq.heappush(h, (node.next.val, i, node.next))
+
+        return head.next
         
-        return dummy.next
-        
+
+            
+
+                
+
