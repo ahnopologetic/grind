51c5f12a6ad4d6ab441a304f86ff11f4bfd1cdf4
ahnsv <ahnsv@bc.edu>
2025-11-12 18:34:11 +0000

    [lc] Runtime - 250 ms (42.85%), Memory - 20 MB (99.99%)

--- lc/0200-number-of-islands/solution.py
+++ lc/0200-number-of-islands/solution.py
@@ -1,36 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [
-            [0, 1],
-            [0, -1],
-            [1, 0],
-            [-1, 0],
-        ]
-        rows = len(grid)
-        cols = len(grid[0])
-        visited = set()
-        num_of_lands = 0
-        
-        def bfs(row, col):
-            q = deque()
-            
-            visited.add((row, col))
-            q.append((row, col))
-
-            while q:
-                row, col = q.popleft()
-                for dr, dc in directions:
-                    # val = grid[row+r][col+c]
-                    r, c = row + dr, col + dc
-                    if 0 <= r < rows and 0 <= c < cols and grid[r][c] == "1" and (r, c) not in visited:
-                        q.append((r, c))
-                        visited.add((r, c))
+        # DFS
+        count = 0
+        def dfs(m, n):
+            grid[m][n] = 0
+            for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                r = m + dr
+                c = n + dc
+                if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == "1":
+                    dfs(r, c)
 
 
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col] == "1" and (row, col) not in visited:
-                    num_of_lands += 1
-                    bfs(row, col)
-        
-        return num_of_lands
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    dfs(i, j)
+                    count += 1
+        return count
