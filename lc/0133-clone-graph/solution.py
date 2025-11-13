9114026059926b950e2432b9e31a2e442e7dd969
ahnsv <ahnsv@bc.edu>
2025-11-13 04:47:51 +0000

    [lc] Runtime - 42 ms (93.13%), Memory - 18.3 MB (100.00%)

--- lc/0133-clone-graph/solution.py
+++ lc/0133-clone-graph/solution.py
@@ -7,24 +7,29 @@ class Node:
 """
 
 from typing import Optional
-from collections import deque
 class Solution:
+
+    def __init__(self):
+        self.visited = {}
+
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
-            return node
-
-        visited = {}
-        q = deque([node])
-        visited[node] = Node(node.val, [])
+            return
         
-        while q:
-            n = q.popleft()
+        if node in self.visited:
+            return self.visited[node]
+        
+        clone_node = Node(node.val)
 
-            for neigh in n.neighbors:
-                if neigh not in visited:
-                    visited[neigh] = Node(neigh.val, [])
-                    q.append(neigh)
-                visited[n].neighbors.append(visited[neigh])
+        self.visited[node] = clone_node
 
-        return visited[node]
+        if node.neighbors:
+            clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
+        
+        return clone_node
+        
 
+        
+        
+
+        
