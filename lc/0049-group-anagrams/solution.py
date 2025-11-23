8c0791e237cd26e103ab12602c569ac9c70eab34
ahnsv <ahnsv@bc.edu>
2025-11-23 20:56:16 +0000

    [lc] Runtime - 15 ms (41.65%), Memory - 20.4 MB (99.99%)

--- lc/0049-group-anagrams/solution.py
+++ lc/0049-group-anagrams/solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = {}
-        result = []
-
-        for str in strs:
-            sorted_str = "".join(sorted(str))
-            if sorted_str not in d:
-                d[sorted_str] = [str]
+        res = {}
+        for s in strs:
+            chars = [""] * 26
+            for ch in s:
+                chars[ord(ch) - ord('a')] += ch
+            anagram = "".join(chars)
+            if anagram in res:
+                res[anagram].append(s)
             else:
-                d[sorted_str] += [str]
-        
-        return [anagram for anagram in d.values()]
+                res[anagram] = [s]
+        return list(res.values())
+
+
 
