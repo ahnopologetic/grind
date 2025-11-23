331ebfbd0a1c6d8c795eb74e1b34e4a9bee35a61
ahnsv <ahnsv@bc.edu>
2025-11-23 20:45:18 +0000

    [lc] Runtime - 10 ms (35.74%), Memory - 19.5 MB (99.91%)

--- lc/0001-two-sum/solution.py
+++ lc/0001-two-sum/solution.py
@@ -1,25 +1,18 @@
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # brute-force
-        # result = []
-        # curr = 0
-
-        # for i in range(len(nums)):
-        #     for j in range(curr + 1, len(nums)):
-        #         if nums[curr] + nums[j] == target:
-        #             result = [curr, j]
-        #     curr += 1
-
-        # return result
-
-        # two-pass hash table
-        d = {}
-        for i in range(len(nums)):
-            d[nums[i]] = i
+        visited = defaultdict(list)
+        for idx, num in enumerate(nums):
+            visited[num] += [idx]
         
-        for j in range(len(nums)):
-            complement = target - nums[j]
-            if complement in d and d[complement] != j:
-                return [j, d[complement]]
+        for i, num in enumerate(nums):
+            if num in visited:
+                if len(visited[num]) == 1:
+                    del visited[num]
+                else:
+                    visited[num].pop(0)
+            complement = target - num
+            if complement in visited:
+                return [i, visited[complement][-1]]
         
-        return []
+        return [0, 1]
