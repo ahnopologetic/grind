bd6032ff5a25a3a6f873314a8e569a982b5bd5bb
ahnsv <ahnsv@bc.edu>
2025-11-24 02:37:57 +0000

    [lc] Runtime - 35 ms (27.19%), Memory - 27.1 MB (32.15%)

--- lc/0238-product-of-array-except-self/solution.py
+++ lc/0238-product-of-array-except-self/solution.py
@@ -1,17 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output = [1] * len(nums)
+        # [1, 2, 3, 4]
+        # [1, 2, 6, 24]
+        # [4, 12, 24, 24]
 
-        left = 1
-        for i in range(len(nums)):
-            output[i] *= left
-            left *= nums[i]
+        length = len(nums)
+        L, R, ans = [0] * length, [0] * length, [0] * length
+
+        L[0] = 1
+        for i in range(1, length):
+            L[i] = nums[i-1] * L[i-1]
         
-        right = 1
-        for i in range(len(nums) -1, -1, -1):
-            output[i] *= right
-            right *= nums[i]
+        R[length - 1] = 1
+        for i in reversed(range(length - 1)):
+            R[i] = nums[i+1] * R[i+1]
         
-        return output
+
+        for i in range(length):
+            ans[i] = L[i] * R[i]
+        
+        return ans
 
 
