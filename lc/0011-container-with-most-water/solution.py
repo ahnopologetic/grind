5f1f4dc82160001c3ef977665d027edcbb5d090f
ahnsv <ahnsv@bc.edu>
2025-11-25 23:47:49 +0000

    [lc] Runtime - 99 ms (5.71%), Memory - 28.4 MB (99.85%)

--- lc/0011-container-with-most-water/solution.py
+++ lc/0011-container-with-most-water/solution.py
@@ -1,13 +1,16 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        l, r = 0, len(height) - 1
+        # find pair such that min(left, right) * (right - left) is max
+        start, end = 0, len(height) - 1
         max_area = float('-inf')
-
-        while l <= r:
-            max_area = max(max_area, (r - l) * min(height[r], height[l]))
-            if height[l] < height[r]:
-                l += 1
+        while start < end:
+            area = min(height[start], height[end]) * (end - start)
+            max_area = max(max_area, area)
+            if height[start] <= height[end]:
+                start += 1
             else:
-                r -= 1
-        
+                end -= 1
         return max_area
+
+
+
