14c30b86fd6e18b86cdb40aa2fc4e93c62b25192
ahnsv <ahnsv@bc.edu>
2025-12-03 16:42:38 +0000

    [lc] Runtime - 50 ms (78.71%), Memory - 19.7 MB (100.00%)

--- lc/0141-linked-list-cycle/solution.py
+++ lc/0141-linked-list-cycle/solution.py
@@ -6,10 +6,16 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # use fast-slow pointer
+        if not head:
+            return False
+        
         fast = slow = head
         while fast and fast.next:
-            fast = fast.next.next
             slow = slow.next
-            if fast == slow:
+            fast = fast.next.next
+
+            if slow == fast:
                 return True
+        
         return False
