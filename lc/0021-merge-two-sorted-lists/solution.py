e5dfc5f6f42e85aad764591fe45bc52a6e6c195d
ahnsv <ahnsv@bc.edu>
2025-12-03 16:40:18 +0000

    [lc] Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

--- lc/0021-merge-two-sorted-lists/solution.py
+++ lc/0021-merge-two-sorted-lists/solution.py
@@ -5,18 +5,32 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        ptr = dummy = ListNode()
+        # create a dummy, while list1 or list2 append smallest to the dummy and move one fwd.
+
+        # we need a tail to append!
+        dummy = ListNode()
+        tail = dummy
+        
+        if not list1 and not list2:
+            return
+
+
         while list1 and list2:
-            if list1.val <= list2.val:
-                ptr.next = list1
+            if list1.val < list2.val:
+                tail.next = list1
                 list1 = list1.next
             else:
-                ptr.next = list2
+                tail.next = list2
                 list2 = list2.next
-            ptr = ptr.next
+            tail = tail.next
         
-        ptr.next = list1 if list2 is None else list2
-
+        if not list1:
+            tail.next = list2
+            tail = tail.next
+        if not list2:
+            tail.next = list1
+            tail = tail.next
+        
         return dummy.next
 
-
+        
