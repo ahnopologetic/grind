a080bf644f6b1803447da619858768349fef1850
ahnsv <ahnsv@bc.edu>
2025-12-03 16:26:30 +0000

    [lc] Runtime - 0 ms (100.00%), Memory - 18.6 MB (100.00%)

--- lc/0206-reverse-linked-list/solution.py
+++ lc/0206-reverse-linked-list/solution.py
@@ -5,11 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node = None
-        while head:
-            temp = head.next
-            head.next = node
-            node = head
-            head = temp
-
-        return node
+        prev = None
+        curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+            
+        return prev
