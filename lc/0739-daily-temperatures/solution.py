1a500a0aaeed0713cebd9c4f1c2a395c4e587ed4
ahnsv <ahnsv@bc.edu>
2025-12-21 16:26:19 +0000

    [lc] Runtime - 107 ms (37.22%), Memory - 32.3 MB (25.06%)

--- lc/0739-daily-temperatures/solution.py
+++ lc/0739-daily-temperatures/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        ans = [0] * n
-        stack = []
+        # monotonic stack: O(n^2) -> O(n)
+        res = [0] * len(temperatures)
+        stack = [] # (temp, idx)
 
-        for curr_day, curr_temp in enumerate(temperatures):
-            while stack and temperatures[stack[-1]] < curr_temp:
-                prev_day = stack.pop()
-                ans[prev_day] = curr_day - prev_day
-            stack.append(curr_day)
-        return ans
+        for idx, temp in enumerate(temperatures):
+            while stack and stack[-1][0] < temp:
+                t, i = stack.pop()
+                res[i] = idx - i
+            stack.append((temp, idx))
+        return res
+            
