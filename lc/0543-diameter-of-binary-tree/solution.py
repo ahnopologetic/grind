db4571d5faa55e98eb659704c95edba18d790774
ahnsv <ahnsv@bc.edu>
2025-12-29 02:33:08 +0000

    [lc] Runtime - 2 ms (81.11%), Memory - 20 MB (100.00%)

--- lc/0543-diameter-of-binary-tree/solution.py
+++ lc/0543-diameter-of-binary-tree/solution.py
@@ -5,22 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.diameter = 0
-
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.iterate_and_update(root)
-        return self.diameter
-    
-    def iterate_and_update(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+        self.res = 0
 
-        left_depth = self.iterate_and_update(root.left)
-        right_depth = self.iterate_and_update(root.right)
+        def dfs(curr):
+            if not curr:
+                return 0
+            
+            left = dfs(curr.left)
+            right = dfs(curr.right)
+            self.res = max(self.res, left + right)
 
-        curr_diameter = left_depth + right_depth
-
-        self.diameter = max(self.diameter, curr_diameter)
-        return max(left_depth, right_depth) + 1
+            return 1 + max(left, right)
+        
+        dfs(root)
+        return self.res
 
