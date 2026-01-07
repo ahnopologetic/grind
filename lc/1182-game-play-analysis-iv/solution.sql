81cf4d62941969ace21a34ce595ca3e57d265087
ahnsv <ahnsv@bc.edu>
2026-01-07 21:42:00 +0000

    [lc] Runtime - 533 ms (40.62%), Memory - 0.0B (100.00%)

--- lc/1182-game-play-analysis-iv/solution.sql
+++ lc/1182-game-play-analysis-iv/solution.sql
@@ -1,7 +1,18 @@
-select ROUND(count(distinct player_id) / (select count(distinct player_id) from activity), 2) fraction
-from activity
-where (player_id, DATE_SUB(event_date, interval 1 day)) in (
-    select player_id, min(event_date) as first_login
-    from activity 
-    group by 1
+-- Write your PostgreSQL query statement below
+with prev as (
+    select *
+        , lag(event_date) over (partition by player_id order by event_date) prev_event_date
+        , row_number() over (partition by player_id order by event_date) rn
+    from activity
 )
+, consec as (
+    select *
+        , event_date - prev_event_date = 1 is_consec
+    from prev
+)
+
+select 
+    round((count(distinct player_id) filter (where is_consec is true and rn = 2))::decimal / count(distinct player_id), 2) fraction
+from consec
+
+
